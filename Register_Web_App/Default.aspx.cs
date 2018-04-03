00fe7bd5e3cf18217a86d28b39e08c455fea9511
QLewis <qplewis@asu.edu>
2018-04-02 17:25:51 -0700

    Can log in as first employee

--- Register_Web_App/Default.aspx.cs
+++ Register_Web_App/Default.aspx.cs
@@ -36,33 +36,25 @@ namespace Register_Web_App
             if (staffOrStudentList.SelectedItem.ToString() == "Staff")
             {
                 //Check that ID is in Employee.xml
-                Response.Redirect("Register.aspx");
+                //Response.Redirect("Register.aspx");
+
+                string fileLocation = Path.Combine(HttpRuntime.AppDomainAppPath, @"App_Data\Employees.xml");
+                XPathDocument dx = new XPathDocument(fileLocation);
+                XPathNavigator nav = dx.CreateNavigator();
+                XPathNodeIterator iterator = nav.Select("/Employees/Employee");
+                iterator.MoveNext(); //Gets student Node
+                XPathNodeIterator it = iterator.Current.Select("ID"); //iterator within an iterator
+                it.MoveNext(); //gets ID
+                testLabel.Text = it.Current.Value;
+                string idNumber = it.Current.Value;
+
+                if (UserName.Text == idNumber)
+                {
+                    Response.Redirect("Register.aspx");
+                }
             }
             else if (staffOrStudentList.SelectedItem.ToString() == "Student")
             {
-                //Check that ID is in Student.xml
-                /*string fileLocation = Path.Combine(HttpRuntime.AppDomainAppPath, @"App_Data\Students.xml");
-                XmlTextReader reader = null;
-                try
-                {
-                    reader = new XmlTextReader(fileLocation);
-                    reader.WhitespaceHandling = WhitespaceHandling.None;
-
-                    reader.ReadStartElement("Students");
-                    reader.Read();
-                    //testLabel.Text = reader.LocalName; //Student
-                    testLabel.Text = reader.ReadElementString("ID");
-                }
-                catch(Exception error)
-                {
-                    Console.Write(error.Message);
-                }
-                finally
-                {
-                    if (reader != null)
-                        reader.Close();
-                }*/
-                //Response.Redirect("studentInfo.aspx");
                 string fileLocation = Path.Combine(HttpRuntime.AppDomainAppPath, @"App_Data\Students.xml");
                 XPathDocument dx = new XPathDocument(fileLocation);
                 XPathNavigator nav = dx.CreateNavigator();
